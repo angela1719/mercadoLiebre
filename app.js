3eb5206c3cb7a0868e6a2b69811724e0c5e094a2
angela1719 <angelarm783@gmail.com>
2022-12-20 20:45:11 -0500

    entrega actividad

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express();
 // app.use('static', express.static(__dirname + '/public'));mas recomendada la de abajo
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto 3000')
-});
+// app.listen(3000, () => {
+//     console.log('Servidor corriendo en puerto 3000')
+// });
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html')
@@ -19,3 +23,6 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html')
 })
+
+
+  
